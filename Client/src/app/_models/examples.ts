aaaf8063c5c3ce8729f6aba1cff0ceba54b215f3
ghecatalina <63772699+ghecatalina@users.noreply.github.com>
2022-04-30 14:27:10 +0300

    Delete Client directory

--- Client/src/app/_models/examples.ts
+++
@@ -1,8 +0,0 @@
-export interface Examples {
-  id?: number;
-  column1?: string;
-  column2?: string;
-  column3?: string;
-  column4?: Date;
-  column5?: Date;
-}
--- Client/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  defaultLang: 'en',
-  forbiddenMessage: "Please stop hacking your way into places where you shouldn't be ok? Thanks!"
-};
